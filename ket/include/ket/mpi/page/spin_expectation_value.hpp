06b1072b7d5126aa027f521b92d82d7ce7ee676a
Naoki Yoshioka <yoshioka.naoki@gmail.com>
2018-08-12 19:09:19 +0900

    Change summation in spin_expectation_value to fix information loss problem

--- ket/include/ket/mpi/page/spin_expectation_value.hpp
+++ ket/include/ket/mpi/page/spin_expectation_value.hpp
@@ -97,30 +97,59 @@ namespace ket
           typedef typename ::ket::utility::meta::real_of<complex_type>::type real_type;
           typedef KET_array<real_type, 3u> spin_type;
           std::vector<spin_type>& spins_in_threads_;
+          std::vector<spin_type>& residuals_in_threads_;
 
           spin_expectation_value_loop_inside(
             RandomAccessIterator const zero_first,
             RandomAccessIterator const one_first,
-            std::vector<spin_type>& spins_in_threads)
-            : zero_first_(zero_first), one_first_(one_first), spins_in_threads_(spins_in_threads)
+            std::vector<spin_type>& spins_in_threads,
+            std::vector<spin_type>& residuals_in_threads)
+            : zero_first_(zero_first),
+              one_first_(one_first),
+              spins_in_threads_(spins_in_threads),
+              residuals_in_threads_(residuals_in_threads)
           { }
 
           template <typename StateInteger>
           void operator()(StateInteger const index, int const thread_index) const
           {
-            typedef
-              typename std::iterator_traits<RandomAccessIterator>::value_type
-              complex_type;
             complex_type const zero_value = *(zero_first_+index);
             complex_type const one_value = *(one_first_+index);
             complex_type const zero_times_one = zero_value*one_value;
 
+            using std::real;
+            real_type const real_zero_times_one
+              = real(zero_times_one) + residuals_in_threads_[thread_index][0u];
+            using std::imag;
+            real_type const imag_zero_times_one
+              = imag(zero_times_one) + residuals_in_threads_[thread_index][1u];
+            using std::norm;
+            real_type const norm_difference
+              = (norm(zero_value) - norm(one_value)) + residuals_in_threads_[thread_index][2u];
+
+            real_type const tmp_x = spins_in_threads_[thread_index][0u] + real_zero_times_one;
+            real_type const tmp_y = spins_in_threads_[thread_index][1u] + imag_zero_times_one;
+            real_type const tmp_z = spins_in_threads_[thread_index][2u] + norm_difference;
+
+            residuals_in_threads_[thread_index][0u]
+              = real_zero_times_one - (tmp_x - spins_in_threads_[thread_index][0u]);
+            residuals_in_threads_[thread_index][1u]
+              = imag_zero_times_one - (tmp_y - spins_in_threads_[thread_index][1u]);
+            residuals_in_threads_[thread_index][2u]
+              = norm_difference - (tmp_z - spins_in_threads_[thread_index][2u]);
+
+            spins_in_threads_[thread_index][0u] = tmp_x;
+            spins_in_threads_[thread_index][1u] = tmp_y;
+            spins_in_threads_[thread_index][2u] = tmp_z;
+
+            /*
             using std::real;
             spins_in_threads_[thread_index][0u] += real(zero_times_one);
             using std::imag;
             spins_in_threads_[thread_index][1u] += imag(zero_times_one);
             using std::norm;
             spins_in_threads_[thread_index][2u] += norm(zero_value) - norm(one_value);
+            */
           }
         };
 
@@ -129,26 +158,50 @@ namespace ket
         make_spin_expectation_value_loop_inside(
           RandomAccessIterator const zero_first,
           RandomAccessIterator const one_first,
-          std::vector<Spin>& spins_in_threads)
+          std::vector<Spin>& spins_in_threads,
+          std::vector<Spin>& residuals_in_threads)
         {
           typedef
             ::ket::mpi::page::spin_expectation_value_detail
               ::spin_expectation_value_loop_inside<RandomAccessIterator>
             result_type;
 
-          return result_type(zero_first, one_first, spins_in_threads);
+          return result_type(zero_first, one_first, spins_in_threads, residuals_in_threads);
         }
 
 
+        template <typename Spin>
         struct spin_expectation_value_accumulate_inside
         {
-          template <typename Spin>
-          Spin operator()(Spin accumulated_spin, Spin const& spin_in_thread) const
+          Spin& residual_;
+
+          explicit spin_expectation_value_accumulate_inside(Spin& residual)
+            : residual_(residual)
+          { }
+
+          Spin operator()(Spin const& accumulated_spin, Spin spin) const
           {
+            spin[0u] += residual_[0u];
+            spin[1u] += residual_[1u];
+            spin[2u] += residual_[2u];
+
+            Spin result = accumulated_spin;
+            result[0u] += spin[0u];
+            result[1u] += spin[1u];
+            result[2u] += spin[2u];
+
+            residual_[0u] = spin[0u] - (result[0u] - accumulated_spin[0u]);
+            residual_[1u] = spin[1u] - (result[1u] - accumulated_spin[1u]);
+            residual_[2u] = spin[2u] - (result[2u] - accumulated_spin[2u]);
+
+            return result;
+
+            /*
             accumulated_spin[0u] += spin_in_thread[0u];
             accumulated_spin[1u] += spin_in_thread[1u];
             accumulated_spin[2u] += spin_in_thread[2u];
             return accumulated_spin;
+            */
           }
         };
 # endif // BOOST_NO_CXX11_LAMBDAS
@@ -185,6 +238,7 @@ namespace ket
         spin_type BOOST_CONSTEXPR_OR_CONST zero_spin = { };
         std::vector<spin_type> spins_in_threads(
           ::ket::utility::num_threads(parallel_policy), zero_spin);
+        std::vector<spin_type> residuals_in_threads(spins_in_threads.size(), zero_spin);
 
         typedef ::ket::mpi::state<Complex, num_page_qubits_, StateAllocator> local_state_type;
         for (std::size_t base_page_id = 0u;
@@ -209,45 +263,90 @@ namespace ket
           loop_n(
             parallel_policy,
             boost::size(zero_page_range),
-            [&zero_page_range, &one_page_range, &spins_in_threads](StateInteger const index, int const thread_index)
+            [&zero_page_range, &one_page_range, &spins_in_threads, &residuals_in_threads](StateInteger const index, int const thread_index)
             {
               Complex const zero_value = *(boost::begin(zero_page_range)+index);
               Complex const one_value = *(boost::begin(one_page_range)+index);
               Complex const zero_times_one = zero_value*one_value;
 
               using std::real;
+              real_type const real_zero_times_one
+                = real(zero_times_one) + residuals_in_threads[thread_index][0u];
+              using std::imag;
+              real_type const imag_zero_times_one
+                = imag(zero_times_one) + residuals_in_threads[thread_index][1u];
+              using std::norm;
+              real_type const norm_difference
+                = (norm(zero_value) - norm(one_value)) + residuals_in_threads[thread_index][2u];
+
+              real_type const tmp_x = spins_in_threads[thread_index][0u] + real_zero_times_one;
+              real_type const tmp_y = spins_in_threads[thread_index][1u] + imag_zero_times_one;
+              real_type const tmp_z = spins_in_threads[thread_index][2u] + norm_difference;
+
+              residuals_in_threads[thread_index][0u]
+                = real_zero_times_one - (tmp_x - spins_in_threads[thread_index][0u]);
+              residuals_in_threads[thread_index][1u]
+                = imag_zero_times_one - (tmp_y - spins_in_threads[thread_index][1u]);
+              residuals_in_threads[thread_index][2u]
+                = norm_difference - (tmp_z - spins_in_threads[thread_index][2u]);
+
+              spins_in_threads[thread_index][0u] = tmp_x;
+              spins_in_threads[thread_index][1u] = tmp_y;
+              spins_in_threads[thread_index][2u] = tmp_z;
+
+              /*
+              using std::real;
               spins_in_threads[thread_index][0u] += real(zero_times_one);
               using std::imag;
               spins_in_threads[thread_index][1u] += imag(zero_times_one);
               using std::norm;
               spins_in_threads[thread_index][2u] += norm(zero_value) - norm(one_value);
+              */
             });
 # else // BOOST_NO_CXX11_LAMBDAS
           loop_n(
             parallel_policy,
             boost::size(zero_page_range),
             ::ket::mpi::page::spin_expectation_value_detail::make_spin_expectation_value_loop_inside(
-              boost::begin(zero_page_range), boost::begin(one_page_range), spins_in_threads));
+              boost::begin(zero_page_range), boost::begin(one_page_range), spins_in_threads, residuals_in_threads));
 # endif // BOOST_NO_CXX11_LAMBDAS
         }
 
+        spin_type residual = zero_spin;
 # ifndef BOOST_NO_CXX11_LAMBDAS
         spin_type spin
           = boost::accumulate(
               spins_in_threads, zero_spin,
-              [](spin_type accumulated_spin, spin_type const& spin_in_thread)
+              [&residual](spin_type const& accumulated_spin, spin_type spin)
               {
+                spin[0u] += residual[0u];
+                spin[1u] += residual[1u];
+                spin[2u] += residual[2u];
+
+                spin_type result = accumulated_spin;
+                result[0u] += spin[0u];
+                result[1u] += spin[1u];
+                result[2u] += spin[2u];
+
+                residual[0u] = spin[0u] - (result[0u] - accumulated_spin[0u]);
+                residual[1u] = spin[1u] - (result[1u] - accumulated_spin[1u]);
+                residual[2u] = spin[2u] - (result[2u] - accumulated_spin[2u]);
+
+                return result;
+
+                /*
                 accumulated_spin[0u] += spin_in_thread[0u];
                 accumulated_spin[1u] += spin_in_thread[1u];
                 accumulated_spin[2u] += spin_in_thread[2u];
                 return accumulated_spin;
+                */
               });
 # else // BOOST_NO_CXX11_LAMBDAS
         spin_type spin
           = boost::accumulate(
               spins_in_threads, zero_spin,
               ::ket::mpi::page::spin_expectation_value_detail
-                ::spin_expectation_value_accumulate_inside());
+                ::make_spin_expectation_value_accumulate_inside(residual));
 # endif // BOOST_NO_CXX11_LAMBDAS
 
         using boost::math::constants::half;
--- ket/include/ket/spin_expectation_value.hpp
+++ ket/include/ket/spin_expectation_value.hpp
@@ -91,18 +91,21 @@ namespace ket
         typedef typename ::ket::utility::meta::real_of<complex_type>::type real_type;
         typedef KET_array<real_type, 3u> spin_type;
         std::vector<spin_type>& spins_in_threads_;
+        std::vector<spin_type>& residuals_in_threads_;
 
         spin_expectation_value_loop_inside(
           RandomAccessIterator const first,
           StateInteger const qubit_mask,
           StateInteger const lower_bits_mask,
           StateInteger const upper_bits_mask,
-          std::vector<spin_type>& spins_in_threads)
+          std::vector<spin_type>& spins_in_threads,
+          std::vector<spin_type>& residuals_in_threads)
           : first_(first),
             qubit_mask_(qubit_mask),
             lower_bits_mask_(lower_bits_mask),
             upper_bits_mask_(upper_bits_mask),
-            spins_in_threads_(spins_in_threads)
+            spins_in_threads_(spins_in_threads),
+            residuals_in_threads_(residuals_in_threads)
         { }
 
         void operator()(StateInteger const value_wo_qubit, int const thread_index) const
@@ -114,17 +117,43 @@ namespace ket
           // xxxxx1xxxxxx
           StateInteger const one_index = zero_index bitor qubit_mask_;
 
-          typedef typename std::iterator_traits<RandomAccessIterator>::value_type complex_type;
           complex_type const zero_value = *(first_+zero_index);
           complex_type const one_value = *(first_+one_index);
           complex_type const zero_times_one = zero_value*one_value;
 
+          using std::real;
+          real_type const real_zero_times_one
+            = real(zero_times_one) + residuals_in_threads_[thread_index][0u];
+          using std::imag;
+          real_type const imag_zero_times_one
+            = imag(zero_times_one) + residuals_in_threads_[thread_index][1u];
+          using std::norm;
+          real_type const norm_difference
+            = (norm(zero_value) - norm(one_value)) + residuals_in_threads_[thread_index][2u];
+
+          real_type const tmp_x = spins_in_threads_[thread_index][0u] + real_zero_times_one;
+          real_type const tmp_y = spins_in_threads_[thread_index][1u] + imag_zero_times_one;
+          real_type const tmp_z = spins_in_threads_[thread_index][2u] + norm_difference;
+
+          residuals_in_threads_[thread_index][0u]
+            = real_zero_times_one - (tmp_x - spins_in_threads_[thread_index][0u]);
+          residuals_in_threads_[thread_index][1u]
+            = imag_zero_times_one - (tmp_y - spins_in_threads_[thread_index][1u]);
+          residuals_in_threads_[thread_index][2u]
+            = norm_difference - (tmp_z - spins_in_threads_[thread_index][2u]);
+
+          spins_in_threads_[thread_index][0u] = tmp_x;
+          spins_in_threads_[thread_index][1u] = tmp_y;
+          spins_in_threads_[thread_index][2u] = tmp_z;
+
+          /*
           using std::real;
           spins_in_threads_[thread_index][0u] += real(zero_times_one);
           using std::imag;
           spins_in_threads_[thread_index][1u] += imag(zero_times_one);
           using std::norm;
           spins_in_threads_[thread_index][2u] += norm(zero_value) - norm(one_value);
+          */
         }
       };
 
@@ -135,24 +164,53 @@ namespace ket
         StateInteger const qubit_mask,
         StateInteger const lower_bits_mask,
         StateInteger const upper_bits_mask,
-        std::vector<Spin>& spins_in_threads)
+        std::vector<Spin>& spins_in_threads,
+        std::vector<Spin>& residuals_in_threads)
       {
         return spin_expectation_value_loop_inside<RandomAccessIterator, StateInteger>(
-          first, qubit_mask, lower_bits_mask, upper_bits_mask, spins_in_threads);
+          first, qubit_mask, lower_bits_mask, upper_bits_mask, spins_in_threads, residuals_in_threads);
       }
 
 
+      template <typename Spin>
       struct spin_expectation_value_accumulate_inside
       {
-        template <typename Spin>
-        Spin operator()(Spin accumulated_spin, Spin const& spin_in_thread) const
+        Spin& residual_;
+
+        explicit spin_expectation_value_accumulate_inside(Spin& residual)
+          : residual_(residual)
+        { }
+
+        Spin operator()(Spin const& accumulated_spin, Spin spin) const
         {
+          spin[0u] += residual_[0u];
+          spin[1u] += residual_[1u];
+          spin[2u] += residual_[2u];
+
+          Spin result = accumulated_spin;
+          result[0u] += spin[0u];
+          result[1u] += spin[1u];
+          result[2u] += spin[2u];
+
+          residual_[0u] = spin[0u] - (result[0u] - accumulated_spin[0u]);
+          residual_[1u] = spin[1u] - (result[1u] - accumulated_spin[1u]);
+          residual_[2u] = spin[2u] - (result[2u] - accumulated_spin[2u]);
+
+          return result;
+
+          /*
           accumulated_spin[0u] += spin_in_thread[0u];
           accumulated_spin[1u] += spin_in_thread[1u];
           accumulated_spin[2u] += spin_in_thread[2u];
           return accumulated_spin;
+          */
         }
       };
+
+      template <typename Spin>
+      inline spin_expectation_value_accumulate_inside<Spin>
+      make_spin_expectation_value_accumulate_inside(Spin& residual)
+      { return spin_expectation_value_accumulate_inside<Spin>(residual); }
 # endif // BOOST_NO_CXX11_LAMBDAS
   } // namespace spin_expectation_value_detail
 
@@ -191,13 +249,15 @@ namespace ket
     spin_type BOOST_CONSTEXPR_OR_CONST zero_spin = { };
     std::vector<spin_type> spins_in_threads(
       ::ket::utility::num_threads(parallel_policy), zero_spin);
+    std::vector<spin_type> residuals_in_threads(spins_in_threads.size(), zero_spin);
 
     using ::ket::utility::loop_n;
 # ifndef BOOST_NO_CXX11_LAMBDAS
     loop_n(
       parallel_policy,
       static_cast<StateInteger>(last-first)/2u,
-      [first, qubit_mask, lower_bits_mask, upper_bits_mask, &spins_in_threads](
+      [first, qubit_mask, lower_bits_mask, upper_bits_mask,
+       &spins_in_threads, &residuals_in_threads](
         StateInteger const value_wo_qubit, int const thread_index)
       {
         // xxxxx0xxxxxx
@@ -207,42 +267,86 @@ namespace ket
         // xxxxx1xxxxxx
         StateInteger const one_index = zero_index bitor qubit_mask;
 
-        typedef typename std::iterator_traits<RandomAccessIterator>::value_type complex_type;
         complex_type const zero_value = *(first+zero_index);
         complex_type const one_value = *(first+one_index);
         complex_type const zero_times_one = zero_value*one_value;
 
+        using std::real;
+        real_type const real_zero_times_one
+          = real(zero_times_one) + residuals_in_threads[thread_index][0u];
+        using std::imag;
+        real_type const imag_zero_times_one
+          = imag(zero_times_one) + residuals_in_threads[thread_index][1u];
+        using std::norm;
+        real_type const norm_difference
+          = (norm(zero_value) - norm(one_value)) + residuals_in_threads[thread_index][2u];
+
+        real_type const tmp_x = spins_in_threads[thread_index][0u] + real_zero_times_one;
+        real_type const tmp_y = spins_in_threads[thread_index][1u] + imag_zero_times_one;
+        real_type const tmp_z = spins_in_threads[thread_index][2u] + norm_difference;
+
+        residuals_in_threads[thread_index][0u]
+          = real_zero_times_one - (tmp_x - spins_in_threads[thread_index][0u]);
+        residuals_in_threads[thread_index][1u]
+          = imag_zero_times_one - (tmp_y - spins_in_threads[thread_index][1u]);
+        residuals_in_threads[thread_index][2u]
+          = norm_difference - (tmp_z - spins_in_threads[thread_index][2u]);
+
+        spins_in_threads[thread_index][0u] = tmp_x;
+        spins_in_threads[thread_index][1u] = tmp_y;
+        spins_in_threads[thread_index][2u] = tmp_z;
+
+        /*
         using std::real;
         spins_in_threads[thread_index][0u] += real(zero_times_one);
         using std::imag;
         spins_in_threads[thread_index][1u] += imag(zero_times_one);
         using std::norm;
         spins_in_threads[thread_index][2u] += norm(zero_value) - norm(one_value);
+        */
       });
 # else // BOOST_NO_CXX11_LAMBDAS
     loop_n(
       parallel_policy,
       static_cast<StateInteger>(last-first)/2u,
       ::ket::spin_expectation_value_detail::make_spin_expectation_value_loop_inside(
-        first, qubit_mask, lower_bits_mask, upper_bits_mask, spins_in_threads));
+        first, qubit_mask, lower_bits_mask, upper_bits_mask, spins_in_threads, residuals_in_threads));
 # endif // BOOST_NO_CXX11_LAMBDAS
 
+    spin_type residual = zero_spin;
 # ifndef BOOST_NO_CXX11_LAMBDAS
     spin_type spin
       = boost::accumulate(
           spins_in_threads, zero_spin,
-          [](spin_type accumulated_spin, spin_type const& spin_in_thread)
+          [&residual](spin_type const& accumulated_spin, spin_type spin)
           {
+            spin[0u] += residual[0u];
+            spin[1u] += residual[1u];
+            spin[2u] += residual[2u];
+
+            spin_type result = accumulated_spin;
+            result[0u] += spin[0u];
+            result[1u] += spin[1u];
+            result[2u] += spin[2u];
+
+            residual[0u] = spin[0u] - (result[0u] - accumulated_spin[0u]);
+            residual[1u] = spin[1u] - (result[1u] - accumulated_spin[1u]);
+            residual[2u] = spin[2u] - (result[2u] - accumulated_spin[2u]);
+
+            return result;
+
+            /*
             accumulated_spin[0u] += spin_in_thread[0u];
             accumulated_spin[1u] += spin_in_thread[1u];
             accumulated_spin[2u] += spin_in_thread[2u];
             return accumulated_spin;
+            */
           });
 # else // BOOST_NO_CXX11_LAMBDAS
     spin_type spin
       = boost::accumulate(
           spins_in_threads, zero_spin,
-          ::ket::spin_expectation_value_detail::spin_expectation_value_accumulate_inside());
+          ::ket::spin_expectation_value_detail::make_spin_expectation_value_accumulate_inside(residual));
 # endif // BOOST_NO_CXX11_LAMBDAS
 
     using boost::math::constants::half;
