123cf225c1bc4b67642e4dbead1185a73116a20c
SanoSatomi <satomi@metro.co.jp>
2024-10-23 11:49:18 +0900

    changed the position of the copyright description

--- interpreter/src/IRGenQASM3Visitor.cpp
+++ interpreter/src/IRGenQASM3Visitor.cpp
@@ -1,8 +1,8 @@
 /// @file IRGenQASM3Visitor.cpp
 /// @brief Visitor to traverse the AST of OpenQASM 3 and generate IR.
-/// @note Include doxygen comments only where used.
 ///
 /// Copyright (c) RIKEN, Japan. All rights reserved.
+/// @note Include doxygen comments only where used.
 
 #include "IRGenQASM3Visitor.h"
 
