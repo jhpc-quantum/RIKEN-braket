44190adb1f6d17dd9e7e6dd8be6786c02c0f449a
Naoki Yoshioka <yoshioka.naoki@gmail.com>
2024-11-07 12:52:34 +0900

    Modify all_in_state_vector to be constexpr

--- ket/include/ket/utility/all_in_state_vector.hpp
+++ ket/include/ket/utility/all_in_state_vector.hpp
@@ -11,12 +11,33 @@ namespace ket
 {
   namespace utility
   {
-    template <typename BitInteger, typename Qubit, typename... Qubits>
-    inline auto all_in_state_vector(BitInteger const num_qubits, Qubit&& qubit, Qubits&&... qubits) -> bool
+# if __cpp_constexpr < 201603
+    namespace all_in_state_vector_detail
     {
+      template <typename BitInteger>
+      struct is_in_state_vector
+      {
+        BitInteger num_qubits_;
+
+        template <typename Qubit>
+        constexpr auto operator()(Qubit&& qubit) const noexcept
+        { return std::forward<Qubit>(qubit) < std::remove_cv_t<std::remove_reference_t<decltype(qubit)>>{num_qubits_}; }
+      }; // struct is_in_state_vector<BitInteger>
+    } // namespace all_in_state_vector_detail
+
+# endif // __cpp_constexpr >= 201603
+    template <typename BitInteger, typename Qubit, typename... Qubits>
+    inline constexpr auto all_in_state_vector(BitInteger const num_qubits, Qubit&& qubit, Qubits&&... qubits) -> bool
+    {
+# if __cpp_constexpr >= 201603
       return ::ket::utility::variadic::all_of(
-        [num_qubits](auto&& qubit) { return qubit < std::remove_cv_t<std::remove_reference_t<decltype(qubit)>>{num_qubits}; },
+        [num_qubits](auto&& qubit) { return std::forward<Qubit>(qubit) < std::remove_cv_t<std::remove_reference_t<decltype(qubit)>>{num_qubits}; },
         std::forward<Qubit>(qubit), std::forward<Qubits>(qubits)...);
+# else // __cpp_constexpr >= 201603
+      return ::ket::utility::variadic::all_of(
+        ::ket::utility::all_in_state_vector_detail::is_in_state_vector<BitInteger>{},
+        std::forward<Qubit>(qubit), std::forward<Qubits>(qubits)...);
+# endif // __cpp_constexpr >= 201603
     }
   } // namespace utility
 } // namespace ket
